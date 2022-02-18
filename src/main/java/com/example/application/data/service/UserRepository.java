19a2812cd642f2523888924a0d9b46d304335854
start.vaadin.com <no-reply@vaadin.com>
2022-02-18 11:32:35 +0000

    Generated project

---
+++ src/main/java/com/example/application/data/service/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.application.data.service;
+
+import com.example.application.data.entity.User;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    User findByUsername(String username);
+}
